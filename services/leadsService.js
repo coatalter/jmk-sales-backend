21ecc9dee97de4c69f448b9e14abfd557e81d71a
coatalter <111971202+coatalter@users.noreply.github.com>
2025-12-05 14:44:24 +0700

    Update leadsService.js

--- services/leadsService.js
+++ services/leadsService.js
@@ -2,7 +2,19 @@ const { Pool } = require('pg');
 
 class LeadsService {
   constructor() {
-    this._pool = new Pool(); 
+    this._pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+
+      ssl: {
+        rejectUnauthorized: false, 
+      },
+    });
   }
 
   async getLeads({ minProbability, job } = {}) {
